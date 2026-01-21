48a77de5cc321f8fa8b2851158ab8a24456e01b7
es-sai-fi <athousandheavenlycats@protonmail.com>
2026-01-20 20:42:38 -0500

    modules/alacritty: init

---
+++ modules/alacritty.nix
@@ -0,0 +1,64 @@
+{adios}:
+let
+  inherit (adios) types;
+in {
+  name = "alacritty";
+
+  inputs = {
+    mkWrapper.path = "/mkWrapper";
+    nixpkgs.path = "/nixpkgs";
+  };
+
+  options = {
+    settings = {
+      type = types.attrs;
+      description = ''
+        Settings to be injected into the wrapped package's `alacritty.toml`.
+
+        See the alacritty documentation:
+        https://alacritty.org/config-alacritty.html
+
+        Disjoint with the `configFile` option.
+      '';
+    };
+    configFile = {
+      type = types.pathLike;
+      description = ''
+        `alacritty.toml` file to be injected into the wrapped package.
+
+        See the alacritty documentation:
+        https://alacritty.org/config-alacritty.html
+
+        Disjoint with the `settings` option.
+      '';
+    };
+
+    package = {
+      type = types.derivation;
+      description = "The alacritty package to be wrapped.";
+      defaultFunc = {inputs}: inputs.nixpkgs.pkgs.alacritty;
+    };
+  };
+
+  impl =
+    { options, inputs }:
+    let
+      inherit (inputs.nixpkgs) pkgs;
+      generator = pkgs.formats.toml {};
+    in
+    assert !(options ? settings && options ? configFile);
+      inputs.mkWrapper {
+        inherit (options) package;
+        preWrap = ''
+          mkdir -p $out/alacritty
+        '';
+        symlinks = {
+          "$out/alacritty/alacritty.toml" =
+            if options ? configFile
+              then options.configFile
+            else if options ? settings
+              then generator.generate "alacritty.toml" options.settings
+            else null;
+        };
+      };
+}
