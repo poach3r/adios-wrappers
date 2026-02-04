f07e3f75fe88c7ca8a40a816abef3b624c89b2ea
poacher <poacherGoneWild@proton.me>
2026-02-04 11:29:56 -0500

    modules/zsh: enable more complex configuration, add plugin support, zoxide and direnv integration
    This is a massive commit which does a number of things:
    1. Allows for `zshrc` and `zshrcFiles` to be used with each other
    2. Allows usage of `extraZshrc` and `extraZshrcFiles` without setting
    their corresponding options
    3. Adds options for configuring aliases, variables, `setopt` options,
    and plugins

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,4 +1,3 @@
-# TODO integration with zoxide, plugins, etc
 { types, ... } @ adios:
 {
   name = "zsh";
@@ -13,39 +12,100 @@
       type = types.string;
       description = ''
         zsh script to be injected into the wrapped package's `.zshrc`.
-
-        Disjoint with the `zshrcFiles` option.
-      '';
-      mutatorType = types.string;
-      mergeFunc = adios.lib.merge.strings.concatLines;
-    };
-    extraZshrc = {
-      type = types.string;
-      description = ''
-        zsh script to be injected at the end of the wrapped package's `.zshrc`.
-
-        Disjoint with the `zshrcFiles` option.
-      '';
-      mutatorType = types.string;
-      mergeFunc = adios.lib.merge.strings.concatLines;
-    };
-    zshrcFiles = {
-      type = types.listOf types.pathLike;
-      description = ''
-        zsh files to be sourced in the wrapped package's `.zshrc` file.
-
-        Disjoint with the `zshrc` option.
       '';
       mutatorType = types.listOf types.pathLike;
       mergeFunc = adios.lib.merge.lists.concat;
     };
+
+    extraZshrc = {
+      type = types.string;
+      description = ''
+        zsh script to be injected after the `zshrcFiles` option in the wrapped 
+        package's `.zshrc`.
+      '';
+      mutatorType = types.listOf types.pathLike;
+      mergeFunc = adios.lib.merge.lists.concat;
+    };
+
+    zshrcFiles = {
+      type = types.listOf types.pathLike;
+      description = ''
+        zsh files to be sourced after the `zshrc` option in the wrapped package's `.zshrc` file.
+      '';
+      mutatorType = types.listOf types.pathLike;
+      mergeFunc = adios.lib.merge.lists.concat;
+    };
+
     extraZshrcFiles = {
       type = types.listOf types.pathLike;
       description = ''
         zsh files to be sourced at the end of the wrapped package's `.zshrc` file.
-
-        Disjoint with the `zshrc` option.:
       '';
+      mutatorType = types.listOf types.pathLike;
+      mergeFunc = adios.lib.merge.lists.concat;
+    };
+
+    settings = {
+      type = types.attrsOf types.bool;
+      description = "";
+    };
+
+    variables = {
+      type = types.attrs;
+      description = ''
+        Variables to be defined in the wrapped package's `.zshrc` file.
+      '';
+    };
+
+    aliases = {
+      type = types.attrsOf (
+        types.union [
+          types.string
+          (types.struct "global alias" {
+            global = types.bool;
+            command = types.string;
+          })
+        ]
+      );
+      description = "";
+    };
+
+    plugins = {
+      type = types.listOf (
+        types.union [
+          types.derivation
+          (types.struct "plugin" {
+            package = types.derivation;
+            path = types.string;
+          })
+        ]
+      );
+      description = ''
+        Plugins to be appended to the wrapped package's `.zshrc`.
+
+        If a value is a derivation, it will be sourced as 
+        `<derivation>/share/<derivation.pname>/<derivation.pname>.zsh` which
+        should be compatible with the majority of plugins. If this path is 
+        incorrect then the value may be defined as an attrSet containing the 
+        attrs `package` and `path` and will be sourced as `<package>/<path>`.
+      '';
+      example =
+        { pkgs }:
+        {
+          plugins = [
+            pkgs.zsh-autosuggestions
+            pkgs.zsh-syntax-highlighting
+            pkgs.zsh-vi-mode
+            {
+              package = pkgs.zsh-fzf-tab;
+              path = "share/fzf-tab/fzf-tab.plugin.zsh";
+            }
+            {
+              package = pkgs.nix-zsh-completions;
+              path = "share/zsh/plugins/nix/nix-zsh-completions.plugin.zsh";
+            }
+          ];
+        };
       mutatorType = types.listOf types.pathLike;
       mergeFunc = adios.lib.merge.lists.concat;
     };
@@ -70,43 +130,82 @@
     { options, inputs }:
     let
       inherit (inputs.nixpkgs.pkgs) writeText;
-      inherit (inputs.nixpkgs.lib) makeBinPath;
-      inherit (builtins) concatStringsSep;
-      sourceFiles = files: (concatStringsSep "\n" (map (file: "source ${file}") files));
+      inherit (inputs.nixpkgs.lib) makeBinPath getExe;
+      inherit (builtins) concatStringsSep attrValues mapAttrs;
+      # Concatenates a list of strings so they're formatted properly for the .zshrc
+      concat = elems: (concatStringsSep "\n" elems) + "\n\n";
+      # Whether a .zshrc should be generated
+      shouldConfigure =
+        options ? zshrc
+        || options ? zshrcFile
+        || options ? plugins
+        || options ? settings
+        || options ? aliases
+        || options ? variables;
+      # Generates zsh sourcing all the files in a list
+      sourceFiles = files: concat (map (file: "source ${file}") files);
       zshrc =
-        if options ? zshrcFiles then
-          (sourceFiles options.zshrcFiles)
-          + (
-            if options ? extraZshrcFiles then
-              "\n" + (sourceFiles options.extraZshrcFiles)
+        let
+          zshrcFiles = if options ? zshrcFiles then sourceFiles "${options.zshrcFiles}\n" else "";
+          extraZshrcFiles = if options ? extraZshrcFiles then "${options.extraZshrcFiles}\n" else "";
+          zshrcText = if options ? zshrc then "${options.zshrc}\n" else "";
+          extraZshrcText = if options ? extraZshrc then "${options.extraZshrc}\n" else "";
+          settings =
+            if options ? settings then
+              concat (
+                attrValues (mapAttrs (name: value: "${if value then "" else "un"}setopt ${name}") options.settings)
+              )
             else
-              ""
-          )
-        else if options ? zshrc then
-          options.zshrc
-          + (
-            if options ? extraZshrc then
-              "\n" + options.extraZshrc
+              "";
+          variables =
+            if options ? variables then
+              concat (attrValues (mapAttrs (name: value: "${name}=${toString value}") options.variables))
             else
-              ""
-          )
-        else
-          null;
+              "";
+          aliases =
+            if options ? aliases then
+              concat (
+                attrValues (
+                  mapAttrs (
+                    name: value:
+                    if value ? global then "alias -g ${name}='${value.command}'" else "alias ${name}='${value}'"
+                  ) options.aliases
+                )
+              )
+            else
+              "";
+          plugins =
+            if options ? plugins then
+              sourceFiles (
+                map (
+                  plugin:
+                  if plugin ? path then
+                    "${plugin.package}/${plugin.path}"
+                  else
+                    "${plugin}/share/${plugin.pname}/${plugin.pname}.zsh"
+                ) options.plugins
+              )
+            else
+              "";
+        in
+        zshrcFiles
+        + zshrcText
+        + variables
+        + aliases
+        + settings
+        + extraZshrcFiles
+        + extraZshrcText
+        + plugins;
     in
-    assert !(options ? zshrc && options ? zshrcFiles);
-    # extraZshrcFiles requires zshrcFiles to be set.
-    assert (if options ? extraZshrcFiles && !(options ? zshrcFiles) then false else true);
-    # extraZshrc requires zshrc to be set.
-    assert (if options ? extraZshrc && !(options ? zshrc) then false else true);
     inputs.mkWrapper {
       inherit (options) package;
       wrapperArgs =
         if options ? extraPackages then "--prefix PATH : ${makeBinPath options.extraPackages}" else null;
       symlinks = {
-        "$out/.zshrc" = if options ? zshrc || options ? zshrcFile then writeText ".zshrc" zshrc else null;
+        "$out/.zshrc" = if shouldConfigure then writeText ".zshrc" zshrc else null;
       };
       environment = {
-        ZDOTDIR = if options ? zshrc || options ? zshrcFile then "$out" else null;
+        ZDOTDIR = if shouldConfigure then "$out" else null;
       };
     };
 }
